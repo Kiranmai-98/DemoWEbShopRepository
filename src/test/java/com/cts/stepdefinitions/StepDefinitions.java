250815270bf6bda100103d1978f129e8cb276b1a
Kiranmai <kiranmai.tummala98@gmail.com>
2020-03-17 18:07:10 +0530

    first commit

--- src/test/java/com/cts/stepdefinitions/StepDefinitions.java
+++ src/test/java/com/cts/stepdefinitions/StepDefinitions.java
@@ -46,139 +46,139 @@ public class StepDefinitions
 		LoginPage.clickOnLogin(driver);
 	}
 	
-//	@When("I recently viewed some items")
-//	public void i_recently_viewed_some_items() 
-//	{
-//	    HomePage.clickOnBooks(driver);
-//	    BooksPage.clickOnFictionBook(driver);
-//	}
-//
-//	@When("click on the recently viewed products")
-//	public void click_on_the_recently_viewed_products() 
-//	{
-//	    HomePage.clickOnRecentlyViewedProducts(driver);
-//	}
-//
-//	@When("click on an item")
-//	public void click_on_an_item() 
-//	{
-//		RecentlyViewedProductsPage.clickOnFiction(driver);
-//	}
-//
-//	@When("click on add to cart")
-//	public void click_on_add_to_cart() 
-//	{
-//		RecentlyViewedProductsPage.clickOnAddTOCart(driver);	    
-//	}
-//	
-//	@Then("The added item should be in the shopping cart")
-//	public void the_added_item_should_be_in_the_shopping_cart() 
-//	{
-//	  
-//	    String actualMsgOfAddedCart = RecentlyViewedProductsPage.getActualMsgOfCarting(driver);
-//	    	
-//	    System.out.println(actualMsgOfAddedCart);
-//	    
-//	    Assert.assertEquals(actualMsgOfAddedCart,"The product has been added to your shopping cart");
-//	
-//	}
-//
-//
-//	@When("I click on add to compare page")
-//	public void i_click_on_add_to_compare_page() 
-//	{
-//	    HomePage.clickOnComputers(driver);	    
-//	    ComputersPage.clickOnDesktop(driver);	    
-//	    ComputersPage.clickOncheapComputer(driver);
-//	    ComputersPage.clickToAddToCompareList(driver);
-//	   
-//	    HomePage.clickOnComputers(driver);
-//	    ComputersPage.clickOnDesktop(driver);
-//	    ComputersPage.clickOnExpensiveComputer(driver);
-//	    ComputersPage.clickToAddToCompareList(driver);
-//	    
-//	}
-//
-//	@Then("They should appear on the compare page")
-//	public void they_should_appear_on_the_compare_page() 
-//	{
-//		HomePage.clickOnCompareProducts(driver);
-//		
-//		if(driver.getPageSource().toLowerCase().contains("build your own cheap computer") &&
-//			   driver.getPageSource().toLowerCase().contains("build your own expensive computer"))
-//		{
-//			System.out.println("Test Passed");
-//		}
-//		else
-//		{
-//			System.out.println("Test failed");
-//		}
-//		
-//		String text1 = ComputersPage.costOfCheapComp(driver);		
-//		String text2 = ComputersPage.costOfExpComp(driver);				
-//		text1 = text1.replace(".00", "");
-//		text2 = text2.replace(".00", "");
-//		int value1 = Integer.parseInt(text1);
-//		int value2 = Integer.parseInt(text2);
-//		if(value1>value2)
-//		{
-//			System.out.println(value1+" is greater");
-//		}
-//		else
-//		{
-//			System.out.println(value2+" is greater");
-//		}
-//	
-//	}
+	@When("I recently viewed some items")
+	public void i_recently_viewed_some_items() 
+	{
+	    HomePage.clickOnBooks(driver);
+	    BooksPage.clickOnFictionBook(driver);
+	}
+
+	@When("click on the recently viewed products")
+	public void click_on_the_recently_viewed_products() 
+	{
+	    HomePage.clickOnRecentlyViewedProducts(driver);
+	}
+
+	@When("click on an item")
+	public void click_on_an_item() 
+	{
+		RecentlyViewedProductsPage.clickOnFiction(driver);
+	}
+
+	@When("click on add to cart")
+	public void click_on_add_to_cart() 
+	{
+		RecentlyViewedProductsPage.clickOnAddTOCart(driver);	    
+	}
 	
-//	@When("click on My account")
-//	public void click_on_My_account() 
-//	{
-//	    HomePage.clickOnMyAccount(driver);
-//	}
-//
-//	@Then("I should be able to edit my information as my name as {String}")
-//	public void i_should_be_able_to_edit_my_information_as_my_name_as(String editedName) 
-//	{
-//		
-//	    MyInfoPage.clearingFirstName(driver);
-//	    MyInfoPage.editingFirstName(driver, editedName);
-//	    MyInfoPage.clickOnSave(driver);
-//	    	    
-//	    String fName = MyInfoPage.toGetEditedName(driver);
-//	    System.out.println(fName);
-//	   
-//	    Assert.assertEquals("Name changed successfully","Bindu",fName);
-//	    	  
-//	}
+	@Then("The added item should be in the shopping cart")
+	public void the_added_item_should_be_in_the_shopping_cart() 
+	{
+	  
+	    String actualMsgOfAddedCart = RecentlyViewedProductsPage.getActualMsgOfCarting(driver);
+	    	
+	    System.out.println(actualMsgOfAddedCart);
+	    
+	    Assert.assertEquals(actualMsgOfAddedCart,"The product has been added to your shopping cart");
+	
+	}
 
 
-//	@When("I Click on Contact as")
-//	public void i_Click_on_Contact_as() 
-//	{
-//	   HomePage.clickOnContactUs(driver);
-//	}
-//	
-//	@When("I enter my name as {String} and email as {String}")
-//	public void i_enter_my_name_as_and_email_as_(String myName,String myMail)
-//	{		
-//	    EnquiryPage.enteringFullName(driver, myName);
-//	    EnquiryPage.enteringEmail(driver, myMail);	   
-//	}
-//
-//	@Then("I can be able to submit an Enquiry as {String}")
-//	public void i_can_be_able_to_submit_an_Enquiry_as_(String enquiryQn) 
-//	{    	    
-//	    EnquiryPage.enterQuery(driver, enquiryQn);
-//	    EnquiryPage.clickOnSubmit(driver);
-//	    
-//	    String actEnqSubMsg = EnquiryPage.getActualSubmittedMsg(driver);
-//	    System.out.println(actEnqSubMsg);
-//	    
-//	    String expEnqSubMsg = "Your enquiry has been successfully sent to the store owner.";
-//	    Assert.assertEquals("Enquiry submitted assertion", expEnqSubMsg, actEnqSubMsg);
-//	    
-//	}
+	@When("I click on add to compare page")
+	public void i_click_on_add_to_compare_page() 
+	{
+	    HomePage.clickOnComputers(driver);	    
+	    ComputersPage.clickOnDesktop(driver);	    
+	    ComputersPage.clickOncheapComputer(driver);
+	    ComputersPage.clickToAddToCompareList(driver);
+	   
+	    HomePage.clickOnComputers(driver);
+	    ComputersPage.clickOnDesktop(driver);
+	    ComputersPage.clickOnExpensiveComputer(driver);
+	    ComputersPage.clickToAddToCompareList(driver);
+	    
+	}
+
+	@Then("They should appear on the compare page")
+	public void they_should_appear_on_the_compare_page() 
+	{
+		HomePage.clickOnCompareProducts(driver);
+		
+		if(driver.getPageSource().toLowerCase().contains("build your own cheap computer") &&
+			   driver.getPageSource().toLowerCase().contains("build your own expensive computer"))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test failed");
+		}
+		
+		String text1 = ComputersPage.costOfCheapComp(driver);		
+		String text2 = ComputersPage.costOfExpComp(driver);				
+		text1 = text1.replace(".00", "");
+		text2 = text2.replace(".00", "");
+		int value1 = Integer.parseInt(text1);
+		int value2 = Integer.parseInt(text2);
+		if(value1>value2)
+		{
+			System.out.println(value1+" is greater");
+		}
+		else
+		{
+			System.out.println(value2+" is greater");
+		}
+	
+	}
+	
+	@When("click on My account")
+	public void click_on_My_account() 
+	{
+	    HomePage.clickOnMyAccount(driver);
+	}
+
+	@Then("I should be able to edit my information as my name as {String}")
+	public void i_should_be_able_to_edit_my_information_as_my_name_as(String editedName) 
+	{
+		
+	    MyInfoPage.clearingFirstName(driver);
+	    MyInfoPage.editingFirstName(driver, editedName);
+	    MyInfoPage.clickOnSave(driver);
+	    	    
+	    String fName = MyInfoPage.toGetEditedName(driver);
+	    System.out.println(fName);
+	   
+	    Assert.assertEquals("Name changed successfully","Bindu",fName);
+	    	  
+	}
+
+
+	@When("I Click on Contact as")
+	public void i_Click_on_Contact_as() 
+	{
+	   HomePage.clickOnContactUs(driver);
+	}
+	
+	@When("I enter my name as {String} and email as {String}")
+	public void i_enter_my_name_as_and_email_as_(String myName,String myMail)
+	{		
+	    EnquiryPage.enteringFullName(driver, myName);
+	    EnquiryPage.enteringEmail(driver, myMail);	   
+	}
+
+	@Then("I can be able to submit an Enquiry as {String}")
+	public void i_can_be_able_to_submit_an_Enquiry_as_(String enquiryQn) 
+	{    	    
+	    EnquiryPage.enterQuery(driver, enquiryQn);
+	    EnquiryPage.clickOnSubmit(driver);
+	    
+	    String actEnqSubMsg = EnquiryPage.getActualSubmittedMsg(driver);
+	    System.out.println(actEnqSubMsg);
+	    
+	    String expEnqSubMsg = "Your enquiry has been successfully sent to the store owner.";
+	    Assert.assertEquals("Enquiry submitted assertion", expEnqSubMsg, actEnqSubMsg);
+	    
+	}
 	
 	@When("I order  anything")
 	public void i_order_anything() 
--- target/classes/feature/Demoshop.feature
+++ target/classes/feature/Demoshop.feature
@@ -7,25 +7,25 @@ Background:
 Given I have the browser with DemoWebShopPage
 When I login to my account using 'bindu.tummala98@gmail.com' as username and 'Bindu@98' as password
 	
-#Scenario: Adding items to cart
-#When I recently viewed some items
-#And click on the recently viewed products
-#And click on an item 
-#And click on add to cart
-#Then The added item should be in the shopping cart
-#
-#Scenario: Comparision of products cost
-#When I click on add to compare page
-#Then They should appear on the compare page
+Scenario: Adding items to cart
+When I recently viewed some items
+And click on the recently viewed products
+And click on an item 
+And click on add to cart
+Then The added item should be in the shopping cart
 
-#Scenario: Editing the information
-#When click on My account
-#Then I should be able to edit my information as my name as 'Bindu'
+Scenario: Comparision of products cost
+When I click on add to compare page
+Then They should appear on the compare page
 
-#Scenario: Submitting an Enquiry
-#When I Click on Contact as
-#And I enter my name as 'Bindu Tummala' and email as 'bindu.tummala98@gmail.com'
-#Then I can be able to submit an Enquiry as 'Please tell about the details of laptops availability..'
+Scenario: Editing the information
+When click on My account
+Then I should be able to edit my information as my name as 'Bindu'
+
+Scenario: Submitting an Enquiry
+When I Click on Contact as
+And I enter my name as 'Bindu Tummala' and email as 'bindu.tummala98@gmail.com'
+Then I can be able to submit an Enquiry as 'Please tell about the details of laptops availability..'
 
 Scenario: To get Details of the order
 When I order  anything
